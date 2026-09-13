7bd783642fca2793f234e3be8b55b75b81e7a9e7
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2026-09-13 09:41:14 -0300

    chore: teste conexão

--- src/dbconfig.js
+++ src/dbconfig.js
@@ -21,7 +21,9 @@ function createDbConnection() {
     /** Cria a tabela no banco de dados */
     createTable(db);
   });
+  
   console.log("Conexão com SQLite foi estabelecida");
+
   return db;
 }
 
@@ -44,5 +46,6 @@ function createTable(db) {
      `);
   }
 
-  module.exports = createDbConnection();
+// Inicializa a conexão e criação da tabela
+module.exports = createDbConnection();
   
